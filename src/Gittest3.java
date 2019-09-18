5e0f207459163503aaa5ee9a4421fa529ac2d290
aonal1905 <onalahmet86@gmail.com>
2019-09-17 20:15:19 -0500

    1 is removed

--- src/Gittest3.java
+++ src/Gittest3.java
@@ -2,7 +2,7 @@ public class Gittest3 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("this is ahmet onal");
-        System.out.println("yay");
+
 
 
     }
